8366acffff9d93c5e58235fd53766f1a9d0254a3
Tim <tim.gelner@gmail.com>
2020-09-14 10:43:14 -0400

    Tried to add boss

--- CVOneFinger/Program.cs
+++ CVOneFinger/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
 
 class Program
 {
+    public static InputSimulator sim = new InputSimulator();
     static Color GetPixel(Point position)
     {
         using (var bitmap = new Bitmap(1, 1))
@@ -20,24 +24,75 @@ class Program
 
     static void Main()
     {
-        while(true)
+        Thread.Sleep(5000);
+        while (true)
         {
-            GetColorNames();
-            Thread.Sleep(1000);
+            if(!CheckBlue() && !CheckRed())
+            {
+                CheckBoss();
+            }
+            
+            Thread.Sleep(300);   
         }        
+    }
+
+    private static bool CheckBoss()
+    {
+        Point point = new Point(1250, 224);
+        Color color = GetPixel(point);
+        if (color.R > 200)
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+            return true;
+        }
+        if (color.B > 200)
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.LEFT);
+            return true;
+        }
+        return false;
+    }
+
+    private static bool CheckRed()
+    {
+        Point point = new Point(1339, 759);
+        Color color = GetPixel(point);
+        if(color.R > 200)
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+            return true;
+        }
+        return false;
+    }
+
+    private static bool CheckBlue()
+    {
+        Point point = new Point(1200, 763);
+        Color color = GetPixel(point);
+        if (color.B > 200)
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.LEFT);
+            return true;
+        }
+        return false;
     }
 
     private static void GetColorNames()
     {
-        Color color = GetPixel(Cursor.Position);
-        Console.WriteLine(color);
+        Point point = Cursor.Position;
+        Color color = GetPixel(point);
+        string s = "At Point: " + point + ", ";
+        s += color;
+        string s1 = "";
         var colorLookup = Enum.GetValues(typeof(KnownColor))
               .Cast<KnownColor>()
               .Select(Color.FromKnownColor)
               .ToLookup(c => c.ToArgb());
         foreach (var namedColor in colorLookup[color.ToArgb()])
         {
-            Console.WriteLine(namedColor.Name);
+            s1 += namedColor.Name + " ";
         }
+        s += " " + s1;
+        Console.WriteLine(s);
     }
 }
